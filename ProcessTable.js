5c60583e0ee1147e70f391596b91b22416101ebd
Chester  Mealer <cdm014@gmail.com>
2023-07-29 11:36:34 -0400

    logging

--- ProcessTable.js
+++ ProcessTable.js
@@ -35,6 +35,8 @@ class ProcessTable {
         ///</Summary>
         console.log("Checking id: "+v_id);
         console.log("processes: "+_.keys(this.processes));
+        let keys = _.keys(this.processes);
+        console.log("found: "+_.contains(keys,v_id));
         if (_.contains(_.keys(this.processes),v_id)) {
             console.log("ID exists in table");
             //check if the process has been killed
