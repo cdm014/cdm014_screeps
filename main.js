f3503dc159f183fda055af368df8fb0dc542bd9f
Chester  Mealer <cdm014@gmail.com>
2023-07-29 10:39:53 -0400

    bugfix?

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var Traveler = require('Traveler');
 let Role = require('CreepRole');
 let _ProcessTable = require('ProcessTable');
 let Process = require('ProcessEntry');
-let ProcessNames = require('ProcessNames');
+//let ProcessNames = require('ProcessNames');
 require('Utils');
 var Empire = require('Empire');
 //var CreepRequest = require('CreepRequest');
@@ -15,7 +15,7 @@ module.exports.loop = function () {
   //spawn.SpawnBootstrapper("main loop");
   var ProcessTable = new _ProcessTable()
   //ALWAYS MAKE SURE WE HAVE THE BOOTSTRAP PROCESS AT PRIORITY 0
-  let Bootstrap = new Process(0, 0, null, ProcessNames.BOOTSTRAP)
+  let Bootstrap = new Process(0, 0, null, "_process_BOOTSTRAP")
   ProcessTable.addProcess(Bootstrap);
 
   var v_Empire = new  Empire();
