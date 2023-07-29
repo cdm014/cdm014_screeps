5d4c666c2f53813654ca2c40525544f816c6c3af
Chester  Mealer <cdm014@gmail.com>
2023-07-29 11:00:08 -0400

    test process

--- ProcessNames.js
+++ ProcessNames.js
@@ -1,5 +1,6 @@
 const ProcessNames = {
     BOOTSTRAP : "_process_BOOTSTRAP", //CRITICAL PROCESS 
+    TEST: "_process_TEST", //PLACEHOLDER PROCESS TO TEST PROCESS TABLE
 
 }
 module.exports = ProcessNames
--- main.js
+++ main.js
@@ -18,6 +18,8 @@ module.exports.loop = function () {
   //ALWAYS MAKE SURE WE HAVE THE BOOTSTRAP PROCESS AT PRIORITY 0
   let Bootstrap = new Process(0, 0,ProcessStatus.RUNNING, null, ProcessNames.BOOTSTRAP)
   ProcessTable.addProcess(Bootstrap);
+  let TestProcess = new Process(1,0,ProcessStatus.INITALIZING,null,ProcessNames.TEST)
+  ProcessTable.addProcess(TestProcess);
 
   var v_Empire = new  Empire();
   console.log("Next Process ID: "+ProcessTable.getNextId())
