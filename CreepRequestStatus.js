4d0cbb07cfd9f32134895ee0c49f948234f67d91
Chester  Mealer <cdm014@gmail.com>
2025-07-20 16:47:17 -0400

    starting over

--- CreepRequestStatus.js
+++
@@ -1,8 +0,0 @@
-const RequestStatus = {
-    NONE: "none",
-    REQUESTED: "requested",
-    REJECTED: "rejected",
-    SPAWNING: "spawning"
-  }
-
-  module.exports = RequestStatus;
--- CreepRole.js
+++
@@ -1,9 +0,0 @@
-const CreepRole = {
-    BOOTSTRAPPER: "bootstrapper",
-    WORKER: "worker",
-    CLAIMER1: "claimer1",
-    CLAIMER2: "claimer2",
-    ATTACKER: "attacker"
-  };
-
-  module.exports = CreepRole;
